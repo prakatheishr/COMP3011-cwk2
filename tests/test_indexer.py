51d3033567c935b8b1fe0c65c0ea4bc8ec78a06b
prakatheishr <sc22pr@leeds.ac.uk>
2026-04-24 11:33:47 +0100

    test: add inverted index frequency position and document frequency tests

--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -44,3 +44,63 @@ def test_tokenize_handles_numbers():
     result = tokenize("Page 10 has 2 quotes")
 
     assert result == ["page", "10", "has", "2", "quotes"]
+
+from src.indexer import build_index
+
+
+def test_build_index_stores_frequency_and_positions():
+    """
+    Index should store word frequency and token positions per page.
+    """
+    pages = [
+        {
+            "url": "page1",
+            "content": "life is life",
+        }
+    ]
+
+    index = build_index(pages)
+
+    assert "life" in index
+    assert index["life"]["document_frequency"] == 1
+    assert index["life"]["pages"]["page1"]["frequency"] == 2
+    assert index["life"]["pages"]["page1"]["positions"] == [0, 2]
+
+
+def test_build_index_stores_document_frequency_once_per_page():
+    """
+    Document frequency should count pages, not total word occurrences.
+    """
+    pages = [
+        {
+            "url": "page1",
+            "content": "life life life",
+        },
+        {
+            "url": "page2",
+            "content": "life again",
+        },
+    ]
+
+    index = build_index(pages)
+
+    assert index["life"]["document_frequency"] == 2
+    assert index["life"]["pages"]["page1"]["frequency"] == 3
+    assert index["life"]["pages"]["page2"]["frequency"] == 1
+
+
+def test_build_index_handles_repeated_words():
+    """
+    Repeated words should increase frequency and store all positions.
+    """
+    pages = [
+        {
+            "url": "page1",
+            "content": "good good friends",
+        }
+    ]
+
+    index = build_index(pages)
+
+    assert index["good"]["pages"]["page1"]["frequency"] == 2
+    assert index["good"]["pages"]["page1"]["positions"] == [0, 1]
